cce6e2700cd8410d41e1b92eb7aacc0057af8344
Illia Komissarov <iliakomissarow99@gmail.com>
2023-11-05 17:09:26 +0200

    basic done

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,20 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+enum WorkDay {
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thr",
+  "Fri",
+}
+enum Weekend {
+  "Sat",
+  "Sun",
+}
+
+const WorkDays = [];
+
+function isWeekend(day: WorkDay | Weekend): boolean {
+  return day === Weekend.Sat || day === Weekend.Sun;
+}
--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,22 +2,32 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
-  title: 'The awesome page',
+type Article = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+const page1: Article = {
+  title: "The awesome page",
   likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
   details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
 
-const page2 = {
-  title: 'Python or Js',
+const page2: Article = {
+  title: "Python or Js",
   likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+  accounts: ["Alex"],
+  status: "close",
+};
 
 export {};
