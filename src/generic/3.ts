c40914662909ee80f5673e65076eda25050a61a4
Illia Komissarov <iliakomissarow99@gmail.com>
2023-11-08 21:27:19 +0200

    task 3

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,7 +3,13 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+// function merge (objA, objB) {
+//   return Object.assign(objA, objB);
+// }
+
+// export {}
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
