fa90176d574a25dd5ca96164255f8f748509d84a
Illia Komissarov <iliakomissarow99@gmail.com>
2023-11-09 21:06:03 +0200

    Update 8.ts

--- src/generic/8.ts
+++ src/generic/8.ts
@@ -3,6 +3,26 @@
   Ви хочете створити новий тип Params, який включає всі поля з Form, крім errors.
 */
 
+// type Errors = {
+//   email?: string[];
+//   firstName?: string[];
+//   lastName?: string[];
+//   phone?: string[];
+// };
+
+// type Form = {
+//   email: string | null;
+//   firstName: string | null;
+//   lastName: string | null;
+//   phone: string | null;
+//   errors: Errors;
+// };
+
+// // Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
+// type Params = Form;
+
+// export {};
+
 type Errors = {
   email?: string[];
   firstName?: string[];
@@ -19,6 +39,6 @@ type Form = {
 };
 
 // Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
-type Params = Form;
+type Params = Omit<Form, "errors">;
 
 export {};
