7ee1b993d6395fcd541d840ab6ed2e2e8944ff7b
Illia Komissarov <iliakomissarow99@gmail.com>
2023-11-09 21:03:56 +0200

    Update 4.ts

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,36 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+// class Component {
+//   constructor (public props:T) {
 
+//   }
+// }
+
+// class Page extends Component {
+//   pageInfo () {
+//     console.log(this.props.title);
+//   }
+// }
+
+// export {};
+
+/*
+  Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
+*/
+interface PageProps {
+  title: string;
+}
+
+class Component<T> {
+  constructor(public props: T) {
   }
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<PageProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
 
-export {};
+export { };
